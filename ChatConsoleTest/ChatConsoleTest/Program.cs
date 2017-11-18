b7afd37fe1d4507fb3e1d271090bd32c5f4a6220
Владислав Бирюков <biryukov.vlad2011@yandex.ru>
2017-11-18 20:51:05 +0500

    create program.cs

--- ChatConsoleTest/ChatConsoleTest/Program.cs
+++ ChatConsoleTest/ChatConsoleTest/Program.cs
@@ -2,14 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
+using ZeroMQ;
 
 namespace ChatConsoleTest
 {
-    class Program
+    static partial class Program
     {
-        static void Main(string[] args)
+        public static void HWServer(string[] args)
         {
+            //
+            // Hello World server
+            //
+            // Author: metadings
+            //
+
+            if (args == null || args.Length < 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Usage: ./{0} HWServer [Name]", AppDomain.CurrentDomain.FriendlyName);
+                Console.WriteLine();
+                Console.WriteLine("    Name   Your name. Default: World");
+                Console.WriteLine();
+                args = new string[] { "World" };
+            }
+
+            string name = args[0];
+
+            // Create
+            using (var context = new ZContext())
+            using (var responder = new ZSocket(context, ZSocketType.REP))
+            {
+                // Bind
+                responder.Bind("tcp://*:5555");
+
+                while (true)
+                {
+                    // Receive
+                    using (ZFrame request = responder.ReceiveFrame())
+                    {
+                        Console.WriteLine("Received {0}", request.ReadString());
+
+                        // Do some work
+                        Thread.Sleep(1);
+
+                        // Send
+                        responder.Send(new ZFrame(name));
+                    }
+                }
+            }
         }
     }
 }
