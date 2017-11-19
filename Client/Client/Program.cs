d4baff9c43d876c48fc3d7739bd05295760d0358
Владислав Бирюков <biryukov.vlad2011@yandex.ru>
2017-11-19 19:11:02 +0500

    version 0.3

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -12,42 +12,43 @@ namespace Examples
             if (args == null || args.Length < 1)
             {
                 Console.WriteLine("Welcome to our chat (kinda)");
-                Console.WriteLine("Write your name, next your message then press Enter button");
                 Console.WriteLine();
                 args = new string[] { "tcp://127.0.0.1:5570" };
             }
 
             string endpoint = args[0];
 
+            string UserName;
+            Console.Write("Enter your name: ");
+            UserName = Console.ReadLine();
+            
             // Create
             using (var context = new ZContext())
             using (var requester = new ZSocket(context, ZSocketType.REQ))
             {
                 // Connect
                 requester.Connect(endpoint);
-                for (int m = 0; m < 1; ++m)
-                     {
-                    string UserName;
-                    UserName = Console.ReadLine();
-                    Console.Write("Sending {0}...", UserName);
+                
+                    
+                for (int n = 0; n < 30; ++n)
+                {
+                    string requestText;
+                    Console.Write("Enter your message: ");
+                    requestText = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.Write("Sending {0}: {1}...", UserName, requestText);
 
-                    for (int n = 0; n < 30; ++n)
+                    // Send
+                    requester.Send(new ZFrame(requestText));
+
+                    // Receive
+                    using (ZFrame reply = requester.ReceiveFrame())
                     {
-                        string requestText;
-                        requestText = Console.ReadLine();
-                        Console.Write("Sending {0}: {1}...", UserName, requestText);
-
-                        // Send
-                        requester.Send(new ZFrame(requestText));
-
-                        // Receive
-                        using (ZFrame reply = requester.ReceiveFrame())
-                        {
-                           // Console.WriteLine(" Received: {0} {1}!", requestText, reply.ReadString());
-                            //Thread.Sleep(10000);
-                        }
+                    Console.WriteLine("Received from {0}: {1} {2}!", UserName, requestText, reply.ReadString());
+                    //Thread.Sleep(10000);
                     }
                 }
+                
             }
         }
     }
